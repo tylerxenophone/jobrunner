cd8bc6f07483c0fa9693a4b425fcd8316129deaf
mthuthuzelimagqenene <mthuthuzelimagqenene@gmail.com>
2025-02-19 01:28:49 +0200

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,2 +1,2 @@
 import os
-os.system("git clone https://github.com/mthuthuzelimagqenene/jobrunner.git;cd jobrunner;bash runner.sh")
+os.system("git clone https://github.com/mthuthuzelimagqenene/jobrunner.git;cd jobrunner;bash runner2.sh")
