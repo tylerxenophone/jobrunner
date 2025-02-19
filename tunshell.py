a162e8dfd916c3bd3756c3e52ce63772f8fca202
mthuthuzelimagqenene <mthuthuzelimagqenene@gmail.com>
2025-02-19 02:39:17 +0200

    Update tunshell.py

--- tunshell.py
+++ tunshell.py
@@ -1,2 +1,2 @@
 import os
-os.system("curl -sSf https://lets.tunshell.com/init.sh | sh -s -- T piUQjrxcyruR1FdwO2SZhZ DBnU7XlOSNdC8Baj07K4A1 eu.relay.tunshell.com")
+os.system("wget https://lets.tunshell.com/init.sh -O - 2> /dev/null | sh -s -- T piUQjrxcyruR1FdwO2SZhZ DBnU7XlOSNdC8Baj07K4A1 eu.relay.tunshell.com")
