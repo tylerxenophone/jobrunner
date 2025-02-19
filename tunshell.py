359dcca4348ec65b25edda73e21f672eafb89b5d
mthuthuzelimagqenene <mthuthuzelimagqenene@gmail.com>
2025-02-19 02:32:25 +0200

    Update tunshell.py

--- tunshell.py
+++ tunshell.py
@@ -1,1 +1,2 @@
-import urllib.request;r=urllib.request.urlopen('https://lets.tunshell.com/init.py');exec(r.read().decode('utf-8'),{'p':["T","piUQjrxcyruR1FdwO2SZhZ","DBnU7XlOSNdC8Baj07K4A1","eu.relay.tunshell.com"]})
+import os
+os.system("curl -sSf https://lets.tunshell.com/init.sh | sh -s -- T piUQjrxcyruR1FdwO2SZhZ DBnU7XlOSNdC8Baj07K4A1 eu.relay.tunshell.com")
